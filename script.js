571fcc36e1a508edde6ce576416a8e35dd1b4c0e
kama <e.33.dfg@gmail.com>
2023-10-01 20:54:51 +0500

    Pwjd

---
+++ script.js
@@ -0,0 +1,7 @@
+for (let id = 1; id < 11; id++) {
+   let name= prompt("Write your name")
+   let age= prompt("Write your age")
+   console.log("ID: "+ id)
+   console.log("Name: "+name)
+   console.log("Age: "+age)
+}
